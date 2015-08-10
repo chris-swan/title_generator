ca75d97b73c9d86f3fd07fed7e8523731275fe44
Chris Swan & Kelli Sommerdyke <student@epicodus.com>
2015-08-10 10:53:03 -0700

    Add third test for one letter

--- tests/TitleCaseGeneratorTest.php
+++ tests/TitleCaseGeneratorTest.php
@@ -5,6 +5,21 @@
     class TitleCaseGeneratorTest extends PHPUnit_Framework_TestCase
     {
 
+//First test: take one letter from user to test Title Case Generator
+        function test_makeTitleCase_oneLetter()
+        {
+            //Arange
+            $test_TitleCaseGenerator = new TitleCaseGenerator;
+            $input = "b";
+
+            //Act
+            $result = $test_TitleCaseGenerator->makeTitleCase($input);
+
+            //Assert
+            $this->assertEquals("B", $result);
+        }
+
+//Second test: take one one word from user to test Title Case Generator
         function test_makeTitleCase_oneWord()
         {
             //Arrange
@@ -12,10 +27,24 @@
             $input = "beowulf";
 
             //Act
-            $result = $test_TItleCaseGenerator->makeTitleCase($input);
+            $result = $test_TitleCaseGenerator->makeTitleCase($input);
 
             //Assert
             $this->assertEquals("Beowulf", $result);
+        }
+
+//Third test: take full sentence from user to test Title Case Generator
+        function test_makeTitleCase_multipleWords()
+        {
+            //Arange
+            $test_TitleCaseGenerator = new TitleCaseGenerator;
+            $input = "the little mermaid";
+
+            //Act
+            $result = $test_TitleCaseGenerator->makeTitleCase($input);
+
+            //Assert
+            $this->assertEquals("The Little Mermaid", $result);
         }
     }
 
